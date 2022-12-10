bc75f1436c99ce5b1e16bacd24a04a4091dfb337
tsaiwei <kk3329188@gmail.com>
2022-12-11 00:27:03 +0800

    [modified] refactory budget query and use TDD to add test

--- src/main/java/com/example/budgetproject/BudgetService.java
+++ src/main/java/com/example/budgetproject/BudgetService.java
@@ -1,98 +1,76 @@
 package com.example.budgetproject;
 
-import org.joda.time.DateTime;
-import org.springframework.stereotype.Service;
-
-import javax.swing.text.html.Option;
-import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
-import java.time.temporal.TemporalAccessor;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 
 public class BudgetService {
-    private  IBudgetRepo budgetRepo;
+    private IBudgetRepo budgetRepo;
+
     public BudgetService(IBudgetRepo budgetRepo) {
-     this.budgetRepo = budgetRepo;
+        this.budgetRepo = budgetRepo;
     }
-    public double Query(LocalDate startTime, LocalDate endTime){
+
+    public double Query(LocalDate startTime, LocalDate endTime) {
         if(endTime.compareTo(startTime)<0){
-            return 0.00;
-        }
-
-       List<Budget> budgets  = budgetRepo.getAll();
-        if (budgets.isEmpty()){
-            return 0.00;
+            return 0;
         }
 
 
-        if(startTime.getYear()==endTime.getYear()&&startTime.getMonth().equals(endTime.getMonth())){
-            int durationDay = endTime.getDayOfMonth() - startTime.getDayOfMonth()+1;
-
-
-            String pk = String.format("%d%d",startTime.getYear(),startTime.getMonth().getValue());
-
-            Optional<Budget> budgetOpt = budgets.stream().filter(b ->
-                b.getYearMonth().equals(pk)).findFirst();
-
-
-            if(budgetOpt.isPresent()){
-                 return calTotalBudget(startTime,budgetOpt.get().getAmount(),durationDay);
-            }else{
-                return 0.00;
-            }
-
-        }else{
-            LocalDate tempStartTime =  startTime;
-            int durationMonth=0;
-            while (tempStartTime.compareTo(endTime)<=0){
-                tempStartTime = tempStartTime.plusMonths(1);
-                durationMonth++;
-            }
-            int em = durationMonth;
-            double amount=0;
-            for(int i=0;i<durationMonth;i++){
-
-                LocalDate tempCalStartTime = startTime.plusMonths(i);
-                LocalDate tempCalEndTime = endTime.plusMonths(i);
-                int durationDay=0;
-                if (tempCalStartTime.getMonth().getValue() == startTime.getMonth().getValue()) {
-                     durationDay = YearMonth.from(tempCalStartTime).lengthOfMonth() - tempCalStartTime.getDayOfMonth()+1;
-                }else if(tempCalStartTime.getMonth().getValue()==endTime.getMonth().getValue()){
-                     durationDay = endTime.getDayOfMonth();
-                }else{
-                    durationDay = YearMonth.from(tempCalStartTime).lengthOfMonth();
+        if (startTime.equals(endTime)) {
+            long durationDay = endTime.toEpochDay() - startTime.toEpochDay() + 1;
+            return calAmount(startTime, durationDay);
+        } else {
+            long durationMonth = YearMonth.from(startTime).until(YearMonth.from(endTime), ChronoUnit.MONTHS);
+            if (durationMonth == 0) {
+                long durationDay = endTime.toEpochDay() - startTime.toEpochDay() + 1;
+                return calAmount(startTime, durationDay);
+            } else {
+                LocalDate tempDate = startTime;
+                double currentAmount = 0;
+                for (int i = 0; i <= durationMonth; i++) {
+                    long durationDay = 0;
+                    if (i == 0) {
+                        durationDay = tempDate.lengthOfMonth() - tempDate.getDayOfMonth() + 1;
+                    } else if (i == durationMonth) {
+                        LocalDate endTimeLastDay = endTime.with(TemporalAdjusters.firstDayOfMonth());
+                        durationDay = endTime.toEpochDay() - endTimeLastDay.toEpochDay()+1;
+                    } else {
+                        durationDay = tempDate.lengthOfMonth();
+                    }
+                    currentAmount = currentAmount + calAmount(tempDate, durationDay);
+                    tempDate = tempDate.plusMonths(1);
                 }
-
-
-
-                String pk = String.format("%d%d",YearMonth.from(tempCalStartTime).getYear(),YearMonth.from(tempCalStartTime).getMonthValue());
-                Optional<Budget> budgetOpt = budgets.stream().filter(b ->
-                        b.getYearMonth().equals(pk)).findFirst();
-                double monthAmount =calTotalBudget(tempCalStartTime,budgetOpt.get().getAmount(),durationDay);
-                amount=amount+monthAmount;
-
+                return currentAmount;
             }
-            return amount;
+
         }
 
-
-
-
-
     }
-    private double calTotalBudget(TemporalAccessor startTime, int budget, int duration){
 
-
-        YearMonth from = YearMonth.from(startTime);
-
-        int days = from.lengthOfMonth();
-
-        return ( (double)duration/ (double)days) *  (double)budget;
+    private double calAmount(LocalDate calDate, long durationDay) {
+        return getTotalBudget(calDate) / YearMonth.from(calDate).lengthOfMonth() * durationDay;
     }
+
+
+    private double getTotalBudget(LocalDate startTime) {
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyyMM");
+        String lookUpKey = startTime.format(dateFormatter);
+        List<Budget> budgetList = budgetRepo.getAll() ;
+        if(budgetList.isEmpty()){
+            return 0;
+        }
+        for (Budget budget : budgetRepo.getAll()) {
+            if (budget.getYearMonth().equals(lookUpKey)) {
+                return budget.getAmount();
+            }
+        }
+        return 0;
+    }
+
 }
--- src/test/java/com/example/budgetproject/BudgetProjectApplicationTests.java
+++ src/test/java/com/example/budgetproject/BudgetProjectApplicationTests.java
@@ -3,11 +3,10 @@ package com.example.budgetproject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.format.datetime.DateFormatter;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -27,18 +26,54 @@ class BudgetProjectApplicationTests {
 
         when(mock.getAll()).thenReturn(Arrays.asList
                 (new Budget("202212",31),
-                        new Budget("20231",3100),
-                        new Budget("20232",280)) );
+                        new Budget("202301",3100),
+                        new Budget("202302",280)) );
     }
 
     @Test
-    public void budget_crossMonth(){
-
-       double d =  budgetService.Query(LocalDate.of(2022,12,30),LocalDate.of(2023,2,10));
-        Assertions.assertEquals(2+3100+100, d);
-
+    public void budget_sameDay(){
+        double actualAmount = selectActualAmount("20221230","20221230");
+        amountShouldBe(1,actualAmount);
+    }
+    @Test
+    public void budget_not_found_in_DB(){
+        double actualAmount = selectActualAmount("20221030","20221130");
+        amountShouldBe(0,actualAmount);
+    }
+    @Test
+    public void budget_different_Day_same_month(){
+        double actualAmount = selectActualAmount("20221201","20221210");
+        amountShouldBe(10,actualAmount);
+    }
+    @Test
+    public void budget_cross_three_month(){
+        double actualAmount = selectActualAmount("20221231","20230202");
+        amountShouldBe(1+3100+20,actualAmount);
+    }
+    @Test
+    public void budget_cross_two_month(){
+        double actualAmount = selectActualAmount("20221231","20230103");
+        amountShouldBe(1+300,actualAmount);
     }
 
+    @Test
+    public void endTime_greaterThen_startTime(){
+        double actualAmount = selectActualAmount("20220202","20220201");
+        amountShouldBe(0,actualAmount);
+    }
+
+
+
+    private double selectActualAmount(String startTime,String endTime) {
+        DateTimeFormatter dateFormatter  = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDate startDate = LocalDate.parse(startTime, dateFormatter);
+        LocalDate endDate = LocalDate.parse(endTime,dateFormatter);
+        return budgetService.Query(startDate,endDate);
+    }
+
+    private static void amountShouldBe(double expect,double actual) {
+        Assertions.assertEquals(expect,actual);
+    }
 
 
 }
